cb3c1ddf85fed8e65d3d794093b158290846a6ca
wooing <wooing27@hotmail.co.jp>
2016-08-14 23:22:55 +0800

    Roughly coding ... 25%

---
+++ src/AntShares/Network/Payloads/AddrPayload.java
@@ -0,0 +1,35 @@
+package AntShares.Network.Payloads;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import AntShares.IO.ISerializable;
+
+class AddrPayload implements ISerializable
+{
+	public Object /* TODO NetworkAddressWithTime*/[] AddressList;
+
+    private AddrPayload(Object[] addressList) {
+		super();
+		AddressList = addressList;
+	}
+
+    public static AddrPayload Create(/*NetworkAddressWithTime*/ Object[] addresses)
+    {
+        return new AddrPayload(addresses);
+    }
+
+    @Override
+    public void Deserialize(InputStream reader)
+    {
+    	// TODO
+        //this.AddressList = reader.ReadSerializableArray<NetworkAddressWithTime>();
+    }
+
+    @Override
+    public void Serialize(OutputStream writer)
+    {
+    	// TODO
+        //writer.Write(AddressList);
+    }
+}
